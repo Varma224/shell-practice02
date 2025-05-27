d348057902e9fd9e11b8f4f2acf29c571acdfeeb
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-27 22:23:50 +0530

    testing special vars

--- shellscript.sh
+++ shellscript.sh
@@ -9,11 +9,18 @@ else
     echo "You are running script with root access"
 fi
 
-dnf install jqajuauj -y
+dnf list installed mysql
 
 if [ $? -eq 0 ]; then
-    echo "Installing MYSQL is success"
+    echo "MYSQL is already installed"
 else
-    echo "Installing MYSQL is failure"
-    exit 1
+    echo "MYSQL is not installed going to install it"
+    dnf install mysql -y
+
+    if [ $? -eq 0 ]; then
+        echo "Installing MYSQL is success"
+    else
+        echo "Installing MYSQL is failure"
+        exit 1
+    fi
 fi
