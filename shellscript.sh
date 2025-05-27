e59563ed39635279d8aeeba4bdab1a9cd61f8b53
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-27 21:06:00 +0530

    testing special vars

--- shellscript.sh
+++ shellscript.sh
@@ -2,9 +2,8 @@
 
 USERID=$(id -u)
 
-if[$USERID -ne 0]
-then
-    echo "ERROR: Please run this script with root access" 
+if [$USERID -ne 0]; then
+    echo "ERROR: Please run this script with root access"
 else
     echo "You are running script with root access"
 fi
