d0071905906d14b9fa7d779b76b0a822cbb9d1c6
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-27 17:56:31 +0530

    testing special vars

--- hello-world.sh
+++ hello-world.sh
@@ -3,8 +3,8 @@
 echo "All args passed in script : $@"
 echo "No of args passed in script: $#"
 echo "present working directory: $PWD"
-echo "User name in script: $USER"
+echo "User name in script: $0"
 echo "Home directory: $HOME"
 echo "Process ID: $$"
-sleep 15 &
+sleep 10 &
 echo "Last process ID: $!"
