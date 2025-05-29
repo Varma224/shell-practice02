2c55b40ff5bc4578e1bb4ad1bc2785a93fe06a5a
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-29 22:46:23 +0530

    testing shell script with tee

--- awscli.sh
+++ awscli.sh
@@ -6,11 +6,7 @@ DOMAIN_NAME="deeps.sbs"
 INSTANCES=("frontend" "redis" "mongodb" "cart" "shipping" "catalogue" "user" "mysql" "rabbitmq" "payment" "dispatch")
 
 for instance in ${INSTANCES[@]}; do
-  INSTANCE_ID=$(
-    aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c83d4f49209ee12d
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]"
-    --query "Instances[0].InstanceId" --output text
-  )
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c83d4f49209ee12d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
   if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
