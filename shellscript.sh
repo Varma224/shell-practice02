402fda534a6eb4ae6032585fa60734c56d951e2b
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-29 15:38:37 +0530

    testing shell script with tee

--- shellscript.sh
+++ shellscript.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python" "nginx" "httpd")
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at : $(date)" &>>$LOG_FILE
@@ -28,33 +29,13 @@ VALIDATE() {
     fi
 }
 
-dnf list installed mysql &>>$LOG_FILE
-
-if [ $? -eq 0 ]; then
-    echo "MYSQL is already installed" | tee -a $LOG_FILE
-else
-    echo "MYSQL is not installed going to install it" | tee -a $LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
-    VALIDATE $? "MYSQL"
-fi
-
-dnf list installed python3 &>>$LOG_FILE
-
-if [ $? -eq 0 ]; then
-    echo "Python3 is already installed" | tee -a $LOG_FILE
-else
-    echo "python3 is not installed going to install it" | tee -a $LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
-    VALIDATE $? "python3"
-
-fi
-
-dnf list installed nginx &>>$LOG_FILE
-
-if [ $? -eq 0 ]; then
-    echo "nginx is already installed" | tee -a $LOG_FILE
-else
-    echo "nginx is not installed going to install it" | tee -a $LOG_FILE
-    dnf install nginx -y &>>$LOG_FILE
-    VALIDATE $? "nginx"
-fi
+for package in ${PACKAGES[@]}; do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -eq 0 ]; then
+        echo "$package is already installed" | tee -a $LOG_FILE
+    else
+        echo "$package is not installed going to install it" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    fi
+done
