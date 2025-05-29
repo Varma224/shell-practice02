cc30331561f851d873c2ae8a66c132705178d83f
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-29 22:35:48 +0530

    testing shell script with tee

--- awscli.sh
+++ awscli.sh
@@ -9,13 +9,13 @@ for instance in ${INSTANCES[@]}; do
   INSTANCE_ID=$(
     aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c83d4f49209ee12d
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]"
-    --query "Instances[0].InstanceID" --output text
+    --query "Instances[0].InstanceId" --output text
   )
 
   if [ $instance != "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "$instance.PrivateIpAddress" --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
   else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "$instance.PublicIpAddress" --output text)
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
   fi
   echo "$instance Ip address is $IP"
 done
