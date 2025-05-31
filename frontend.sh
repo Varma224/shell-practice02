0478a11d95cf975918a0ed324b127d2502577782
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-31 16:24:43 +0530

    testing shell script with tee

--- frontend.sh
+++ frontend.sh
@@ -29,34 +29,34 @@ VALIDATE() {
     fi
 }
 
-dnf module disable nginx -y >>$LOG_FILE
+dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling default Nginx repo"
 
-dnf module enable nginx:1.24 -y >>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Nginx repo"
 
-dnf install nginx -y >>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx >>$LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx
 VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "removing default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >>$LOG_FILE
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip >>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
 
-rm -rf /etc/nginx/nginx.conf >>$LOG_FILE
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "removing default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "copying nginx.conf"
 
-systemctl restart nginx >>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting nginx"
