e39921dffdfc161e8adc4bf837bce29b614e4c2d
Anudeep varma <datlanagaanudeep@gmail.com>
2025-05-31 13:52:43 +0530

    testing shell script with tee

--- redis.sh
+++ redis.sh
@@ -51,4 +51,4 @@ VALIDATE $? "Started redis"
 END_TIME=$(date +%s) >>$LOG_FILE
 TOTAL_TIME=$(($END_TIME - $START_TIME))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
