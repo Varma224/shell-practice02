d80f8caf9c7f4bf09f6ae86cd76aa1f764f43983
Anudeep varma <datlanagaanudeep@gmail.com>
2025-06-02 22:51:30 +0530

    simplify code

--- mongodb.sh
+++ mongodb.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+source ./common.sh
+app_name=mongodb
 
-mkdir -p $LOGS_FOLDER
-echo "script started executing at : $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running script with root access" | tee -a $LOG_FILE
-fi
-
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is $R failure $N " | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+check_root
+VALIDATE
 
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying Mongodb repo"
@@ -45,3 +23,5 @@ VALIDATE $? "Editing Mongodb conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting Mongodb"
+
+print_time
