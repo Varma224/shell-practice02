c713690903763e4087dfed267ca219771b423ba4
Anudeep varma <datlanagaanudeep@gmail.com>
2025-06-02 23:05:14 +0530

    simplify code

--- catalogue.sh
+++ catalogue.sh
@@ -1,70 +1,9 @@
-#!/bin/bash
+source ./common.sh
+app_name=mongodb
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at : $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running script with root access" | tee -a $LOG_FILE
-fi
-
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is $R failure $N " | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default Nodejs repo"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Nodejs-20 repo"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing Nodejs"
-
-id roboshop
-if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop already exists... $Y SKIPPING $N"
-fi
-
-mkdir -p /app
-VALIDATE $? "Creating app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Catalogue"
-
-rm -rf /app/*
-cd /app
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing Dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue &>>$LOG_FILE
-systemctl start catalogue &>>$LOG_FILE
-VALIDATE $? "Starting Catalogue"
+check_root
+app_setup
+nodejs_setup
 
 cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
@@ -77,3 +16,5 @@ if [ $STATUS -lt 0 ]; then
 else
     echo -e "Data is already loaded... $Y SKIPPING $N"
 fi
+
+print_time
