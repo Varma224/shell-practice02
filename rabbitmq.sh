12b80a434962dfefd659fb7c9f5120821c272845
Anudeep varma <datlanagaanudeep@gmail.com>
2025-06-02 13:00:57 +0530

    testing shell script with tee

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -32,7 +32,7 @@ VALIDATE() {
     fi
 }
 
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+cp rabitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
