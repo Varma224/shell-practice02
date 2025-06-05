c57ec530f62dbbe5d83383562b8abdcd3638a6ae
Anudeep varma <datlanagaanudeep@gmail.com>
2025-06-05 18:25:59 +0530

    simplify code

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ DEST_DIR=$2
 DAYS=${3:-14}
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 FILES_LIST="/tmp/files-to-zip.txt"
 
 R="\e[31m"
