e207536da4444a71de93f842d8e61b9bc542e66b
Anudeep varma <datlanagaanudeep@gmail.com>
2025-06-06 13:13:16 +0530

    simplify code

---
+++ diskutilization.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DISK_USAGE = "df -hT | grep -v Filesystem"
+DISK_THRESHOLD = 1
+MSG = ""
+
+while IFS = read line; do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MSG+="High disk usage on $PARTITION : $USAGE \n"
+    fi
+done <<<DISK_USAGE
